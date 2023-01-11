5019c09e72b77d234f7e475169b8ec7378748b7c
brunovonkoln <brunovonkoln@gmail.com>
2023-01-11 17:44:37 -0300

    ..

---
+++ plus/data.js
@@ -0,0 +1,93 @@
+export default [
+  {
+    title:"01 - DJ Alberto - Straight Down On The Floor",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "01-DJAlberto-StraightDownOnTheFloor.mp3",
+  },
+  {
+    title:"02 - DJ Alberto - The DJ Rocks",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "02-DJAlberto-TheDJRocks.mp3",
+  },
+  {
+    title:"03 - DJ Albert - Control (Phoenix Version)",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "03-DJAlberto-Control(PhoenixVersion).mp3",
+  },
+  {
+    title:"04 - DJAlberto - Donklap",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "04-DJAlberto-Donklap.mp3",
+  },
+  {
+    title:"05 - DJ Alberto - Listen",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "05-DJAlberto-Listen.mp3",
+  },
+  {
+    title:"06 - DJ Alberto - Party Time",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "06-DJAlberto-PartyTime.mp3",
+  },
+  {
+    title:"07 - DJ Alberto - On The Beat",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "07-DJAlberto-OnTheBeat.mp3",
+  },
+  {
+    title:"08 - DJ Alberto - Heartbreaker",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "08-DJAlberto-Heartbreaker.mp3",
+  },
+  {
+    title:"09 - DJ Alberto - Do That Shit",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "09-DJAlberto-DoThatShit.mp3",
+  },
+  {
+    title:"10 - DJ Alberto - Trip",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "10-DJAlberto-Trip.mp3",
+  },
+  {
+    title:"11 - DJ Alberto - The Music Flows",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "11-DJAlberto-TheMusicFlows.mp3",
+  },
+  {
+    title:"12 - DJ Alberto - Blow Up My Mind",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "12-DJAlberto-BlowUpMyMind.mp3",
+  },
+  {
+    title:"13 - DJ Alberto - Concept Motion Megamix",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "13-DJAlberto-ConceptMotionMegamix.mp3",
+  },
+  {
+    title:"14 - DJ Alberto - On The Beat (House Version)",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "14-DJAlberto-OnTheBeat(HouseVersion).mp3",
+  },
+  {
+    title:"15 - DJAlberto - Technoises",
+    artist: "DJ Alberto",
+    cover: "alberto.jpg",
+    file: "15-DJAlberto-Technoises.mp3",
+  },
+  
+];
